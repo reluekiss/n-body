c27c6302aaf318c70a9ab296fa278d843690d9fb
relue <natalierogers048@gmail.com>
2025-02-13 16:06:11 -0500

    curvy space

--- main.c
+++ main.c
@@ -30,6 +30,17 @@ void AddParticle(Vector2 pos, Vector2 vel, float mass) {
     }
 }
 
+float ComputeCurvature(float x, float y) {
+    float curvature = 0.0f;
+    for (int i = 0; i < particleCount; i++) {
+        float dx = x - particles[i].pos.x;
+        float dy = y - particles[i].pos.y;
+        float r = sqrtf(dx * dx + dy * dy + SOFTENING);
+        curvature += 1.0f / r;
+    }
+    return curvature;
+}
+
 int main(int argc, char *argv[]) {
     int initialCount = 50;
     float mass = 250000000.0f;
@@ -116,6 +127,32 @@ int main(int argc, char *argv[]) {
 
         BeginDrawing();
         ClearBackground(WHITE);
+
+#ifdef GRID        
+        float gridSpacing = 20.0f;
+        float sampleStep = 10.0f;
+        float scale = 50.0f;  // adjust to control curvature exaggeration
+
+        for (float y = 0; y <= screenHeight; y += gridSpacing) {
+            Vector2 prev = { 0, y + scale * ComputeCurvature(0, y) };
+            for (float x = sampleStep; x <= screenWidth; x += sampleStep) {
+                float offset = scale * ComputeCurvature(x, y);
+                Vector2 curr = { x, y + offset };
+                DrawLineV(prev, curr, LIGHTGRAY);
+                prev = curr;
+            }
+        }
+        
+        for (float x = 0; x <= screenWidth; x += gridSpacing) {
+            Vector2 prev = { x + scale * ComputeCurvature(x, 0), 0 };
+            for (float y = sampleStep; y <= screenHeight; y += sampleStep) {
+                float offset = scale * ComputeCurvature(x, y);
+                Vector2 curr = { x + offset, y };
+                DrawLineV(prev, curr, LIGHTGRAY);
+                prev = curr;
+            }
+        }
+#endif // GRID
 
         for (int i = 0; i < particleCount; i++) {
             int count = particles[i].trailCount < MAX_TRAIL ? particles[i].trailCount : MAX_TRAIL;
